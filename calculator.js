3b062287fb9dc1a5aae824061d3c930047a22efe
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-03-04 11:17:07 +0100

    add changes

--- calculator.js
+++ calculator.js
@@ -3,5 +3,10 @@ function sum(a, b) {
   return result;
 }
 
+function divide(a, b) {
+  return a / b;
+}
+
 // Im Browser: export default sum
-module.exports = sum;
+module.exports.sum = sum;
+module.exports.divide = divide;
--- calculator.test.js
+++ calculator.test.js
@@ -2,5 +2,12 @@ const sum = require("./calculator");
 
 test("Die Summe von 2 + 5 ist 7", () => {
   const result = sum(2, 5);
+
   expect(result).toEqual(7);
+});
+
+test('Die Summe von "Apfel" + 5 ist "Apfel5"', () => {
+  const result = sum("Apfel", 5);
+
+  expect(result).toEqual("Apfel5");
 });
