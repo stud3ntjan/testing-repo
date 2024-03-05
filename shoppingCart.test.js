3dffbe323f615ee71815cc0e30520b371c616fb3
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-03-05 14:07:52 +0100

    update shoppingcart.test

--- shoppingCart.test.js
+++ shoppingCart.test.js
@@ -1,17 +1,23 @@
 const shoppingCart = require("./shoppingCart");
 
-test("Der Gesamtwert des Cart ist bei 0 Items gleich 0", () => {
-  expect(shoppingCart.getTotalValue()).toBe(0);
-});
+describe("Shopping Cart Tests", () => {
+  test("Der Gesamtwert des Cart ist bei 0 Items gleich 0", () => {
+    expect(shoppingCart.getTotalValue()).toBe(0);
+  });
 
-test("Nach dem ersten addCartItem() muss die Array-Laenge 1 sein", () => {
-  shoppingCart.addCartItem("Apfel", 5);
+  test("Nach dem ersten addCartItem() muss die Array-Laenge 1 sein", () => {
+    // ACT
+    shoppingCart.addCartItem("Apfel", 5);
 
-  expect(shoppingCart.cart.length).toBe(1);
-});
+    //ASSERT
+    expect(shoppingCart.cart.length).toBe(1);
+  });
 
-test("Nach dem zweiten addCartItem() muss die Array-Laenge 2 sein", () => {
-  shoppingCart.addCartItem("Birne", 3);
+  test("Nach dem zweiten addCartItem() muss die Array-Laenge 2 sein", () => {
+    // ACT
+    shoppingCart.addCartItem("Birne", 3);
 
-  expect(shoppingCart.cart.length).toBe(2);
+    // ASSERT
+    expect(shoppingCart.cart.length).toBe(2);
+  });
 });
