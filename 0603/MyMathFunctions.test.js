3d01a41325cee892d7b9bbbba9f8ee7a0e785c31
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-03-06 10:48:52 +0100

    add files

---
+++ 0603/MyMathFunctions.test.js
@@ -0,0 +1,17 @@
+const { addNumbers, divideNrs } = require("./MyMathFunctions");
+
+describe("Test Basic Math Add Functions", () => {
+  test("Basic Add Function to pass with Basic input", () => {
+    const result = addNumbers(2, 5);
+    console.log("My Result", result);
+    expect(result).toBe(7);
+  });
+});
+
+describe("Test Basic Math Divide Functions", () => {
+  test("Basic Divide Function to pass with Basic input", () => {
+    const result = divideNrs(10, 5);
+    console.log("MY division result", result);
+    expect(result).toBe(2);
+  });
+});
